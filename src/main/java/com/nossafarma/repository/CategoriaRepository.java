b401d495ab7d009756d72fe4e46fdc24eabd668c
Geovani Ferreira <gugaheadbanger@gmail.com>
2021-12-08 16:27:44 -0300

    Estrutura finalizada, falta povoar o banco:memo:

--- src/main/java/com/nossafarma/repository/CategoriaRepository.java
+++ src/main/java/com/nossafarma/repository/CategoriaRepository.java
@@ -1,10 +1,13 @@
 package com.nossafarma.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.nossafarma.model.Categoria;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 
-	
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
 	
 }
